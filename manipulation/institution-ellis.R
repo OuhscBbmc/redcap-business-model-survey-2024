470587f155948af26c6af3f4a10a1f10fa31e5e5
Will Beasley <wibeasley@hotmail.com>
2024-09-16 20:24:21 -0500

    `inst4_manage_satisfied`

--- manipulation/institution-ellis.R
+++ manipulation/institution-ellis.R
@@ -404,7 +404,7 @@ ds <-
   ) |>
   map_to_radio(  "inst4_charge_effort") |>
   map_to_radio(  "inst4_charge_success") |>
-  # map_to_checkbox(  "inst4_manage_satisfied") |>
+  map_to_radio(  "inst4_manage_satisfied") |>
   # map_to_checkbox(  "inst4_regulatory") |>
   # map_to_checkbox(  "inst4_regulatory_charge") |>
   # map_to_checkbox(  "inst4_validation") |>
@@ -557,9 +557,9 @@ checkmate::assert_logical(  ds$inst4_charge_reason_additional_fte      , any.mis
 checkmate::assert_logical(  ds$inst4_charge_reason_support_specific    , any.missing=F                       )
 checkmate::assert_logical(  ds$inst4_charge_reason_other               , any.missing=F                       )
 checkmate::assert_integer(  ds$inst4_charge_staff_count      , any.missing=T , lower=0, upper=15   )
-checkmate::assert_factor(  ds$inst4_charge_effort           , any.missing=T )
-checkmate::assert_factor(  ds$inst4_charge_success          , any.missing=T )
-# checkmate::assert_integer(  ds$inst4_manage_satisfied        , any.missing=T , lower=1, upper=5    )
+checkmate::assert_factor(   ds$inst4_charge_effort           , any.missing=T )
+checkmate::assert_factor(   ds$inst4_charge_success          , any.missing=T )
+checkmate::assert_factor(   ds$inst4_manage_satisfied        , any.missing=T )
 # checkmate::assert_character(ds$inst4_regulatory              , any.missing=T , pattern="^.{1,11}$" )
 # checkmate::assert_integer(  ds$inst4_regulatory_charge       , any.missing=T , lower=1, upper=99   )
 # checkmate::assert_integer(  ds$inst4_validation              , any.missing=T , lower=0, upper=2    )
